bd695352f3c51fa68fb3f48b3b3cf1faadf9a394
市野　那由他 <67714705+nayuta-ichino@users.noreply.github.com>
2020-10-18 22:02:27 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'users/new'
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"
+
+  root to: "rooms#index"
   resources :users, only: [:edit , :update]
   resources :rooms, only: [:new , :create]
   
